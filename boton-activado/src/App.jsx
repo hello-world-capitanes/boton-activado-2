419ad43c9d977e22f10908f58beea551fd19ff9d
Nicole Struggia <nicole.struggia@babelgroup.com>
2023-09-29 10:42:46 +0200

    adding footer

--- boton-activado/src/App.jsx
+++ boton-activado/src/App.jsx
@@ -1,9 +1,11 @@
+import Footer from './components/Footer'
 import Switch from './components/Switch'
 
 function App () {
   return (
     <>
       <Switch />
+      <Footer/>
     </>
   )
 }
---
+++ boton-activado/src/components/Footer.jsx
@@ -0,0 +1,14 @@
+import React from 'react'
+import logo from '../assets/babel.png'
+
+const Footer = () => {
+  return (
+    <footer className='footer'>
+      <figure className='footer_imageContainer'>
+        <img src={logo} alt="logo de babel" />
+      </figure>
+    </footer>
+  )
+}
+
+export default Footer
